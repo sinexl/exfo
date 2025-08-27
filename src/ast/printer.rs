6e1b3ed651d2abdc2d22b5d899e50896fbc20315
sinexl <m.yagodnikov@gmail.com>
2025-08-27 19:11:36 +0300

    Rewrite printer.rs

--- src/ast/printer.rs
+++ src/ast/printer.rs
@@ -1,44 +1,40 @@
-use crate::ast::{Expression, ExpressionKind, ExpressionVisitor};
+use crate::ast::{Expression, ExpressionKind};
 use std::fmt::{Display, Formatter, Write};
 
-pub(crate) struct AstPrinter {}
-
-impl ExpressionVisitor<String> for AstPrinter {
-    fn visit(&mut self, expression: &Expression) -> String {
-        let mut str = String::new();
-        self.print(expression, &mut str, 0).unwrap();
-        str
+pub fn print_ast(expr: &Expression<'_>, f: &mut impl Write, indent: usize) -> std::fmt::Result {
+    let tab = " ".repeat((indent + 1) * 2);
+    match &expr.kind {
+        ExpressionKind::Literal(ident) => writeln!(f, "Literal({})", ident),
+        ExpressionKind::Binop { left, right, kind } => {
+            writeln!(f, "{}", kind.name())?;
+            write!(f, "{tab}left = {}", Print(left, indent + 1))?;
+            write!(f, "{tab}right = {}", Print(right, indent + 1))
+        }
+        ExpressionKind::Assignment { taget, value } => {
+            writeln!(f, "Assignment")?;
+            write!(f, "{tab}target = {}", Print(taget, indent + 1))
+        }
+        ExpressionKind::Unary { item, operator } => {
+            writeln!(f, "{}", operator.name())?;
+            write!(f, "{tab}item = {}", Print(item, indent + 1))
+        }
+        ExpressionKind::Grouping(inner) => {
+            writeln!(f, "Grouping")?;
+            write!(f, "{tab}{}", Print(inner, indent + 1))
+        }
     }
 }
 
-impl AstPrinter {
-    #[allow(clippy::only_used_in_recursion)]
-    pub fn print(&self, expr: &Expression<'_>, f: &mut impl Write, indent: usize) -> std::fmt::Result {
-        let tab = " ".repeat((indent + 1) * 2);
-        match &expr.kind {
-            ExpressionKind::Literal(ident) => writeln!(f, "Literal({})", ident),
-            ExpressionKind::Binop { left, right, kind } => {
-                writeln!(f, "{}", kind.name())?;
-                write!(f, "{tab}left = ")?;
-                self.print(left, f, indent + 1)?;
-                write!(f, "{tab}right = ")?;
-                self.print(right, f, indent + 1)
-            }
-            ExpressionKind::Unary { item, operator } => {
-                writeln!(f, "{}", operator.name())?;
-                write!(f, "{tab}item = ")?;
-                self.print(item, f, indent + 1)
-            }
-            ExpressionKind::Grouping(inner) => {
-                writeln!(f, "Grouping")?;
-                self.print(inner, f, indent + 1)
-            }
-        }
+struct Print<'a>(&'a Expression<'a>, usize);
+impl Display for Print<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let Print(expr, ind) = self;
+        print_ast(expr, f, *ind)
     }
 }
 
 impl Display for Expression<'_> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        AstPrinter{}.print(self, f, 0)
+        Print(self, 0).fmt(f)
     }
 }
