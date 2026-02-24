db411d63d98aa64b6e19d8306557748a67ffb9cc
sinexl <m.yagodnikov@gmail.com>
2026-02-24 23:22:12 +0200

    Common: Introduce SymbolTable, which will implement typestate pattern for all symbol tables in compiler passes. In the future, all compiler passes after Resolver would not "emulate" scopes, but only store a "flat" symbol table with appropriate information.

--- src/common.rs
+++ src/common.rs
@@ -1,8 +1,11 @@
+use crate::ast::expression::SymId;
 use crate::lexing::token::Token;
 use bumpalo::Bump;
 use std::fmt;
-use std::fmt::{Display, Formatter};
+use std::fmt::{Debug, Display, Formatter};
 use std::hash::Hash;
+use std::mem::zeroed;
+use std::ops::{Index, IndexMut};
 use std::rc::Rc;
 
 #[derive(Debug, Hash, Default, Clone, Eq, PartialEq)]
@@ -171,7 +174,7 @@ macro_rules! debug_scopes {
             println!("---- Scope {index} ----");
             debug_scope!(scope);
         }
-    }}
+    }};
 }
 
 #[macro_export]
@@ -180,5 +183,56 @@ macro_rules! debug_scope {
         for (name, resolutions) in $scope {
             println!("{name} = {resolutions:?}");
         }
-    }}
+    }};
+}
+
+pub trait CompilerEntity: Debug {}
+
+pub struct SymbolTable<T: CompilerEntity> {
+    inner: Vec<T>,
+}
+
+impl<T: CompilerEntity> SymbolTable<T> {
+    pub fn new(entity_amount: usize) -> Self {
+        let mut s = Self {
+            inner: Vec::with_capacity(entity_amount),
+        };
+        // s.inner.resize(entity_amount, Default::default());
+        unsafe {
+            s.inner.resize_with(entity_amount, || zeroed::<T>());
+        }
+        s
+    }
+
+    pub fn insert(&mut self, sym_id: SymId, value: T) {
+        self[sym_id] = value
+    }
+}
+
+impl<T: CompilerEntity> Index<SymId> for SymbolTable<T> {
+    type Output = T;
+    fn index(&self, index: SymId) -> &Self::Output {
+        let SymId(idx) = index;
+        &self.inner[idx]
+    }
+}
+
+impl<T: CompilerEntity> IndexMut<SymId> for SymbolTable<T> {
+    fn index_mut(&mut self, index: SymId) -> &mut Self::Output {
+        let SymId(idx) = index;
+        &mut self.inner[idx]
+    }
+}
+
+impl<T: CompilerEntity> Debug for SymbolTable<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_list()
+            .entries(
+                self.inner
+                    .iter()
+                    .enumerate()
+                    .map(|(i, e)| format!("{i} => {e:?}")),
+            )
+            .finish()
+    }
 }
